e6efc13dec85f85b107a1c24f2d5aff195e1a29b
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-22 16:14:29 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">
+ Random User
+
+## 💻 Project
+
+- Learning ReactJS.
+
+- A simple project fetching an (external API)[https://randomuser.me/] for users informations, handling _mouseOver event_ to dynamic change and display some state values.
+
+<p>&nbsp;&nbsp;</p>
+
+---
+
+## 🔥 Check it out!
+
+</br>
+
+- [Demo](https://app-random-user.netlify.app/)
+<p align="center">
+  <img src="src/images/user-gif.gif" />
+</p>
+
+<p>&nbsp;&nbsp;</p>
+
+---
+
+## 🚀 Getting started
+
+Clone this repository and run the following commands inside the project folder:
+
+1. `npm install` or `yarn`;
+2. `yarn start`;
+
+To view the project you can open `http://localhost:3000`.
+
+<p>&nbsp;&nbsp;</p>
+
+---
---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from 'react';
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
+
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/women/3.jpg';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState('name');
+  const [value, setValue] = useState('random person');
+
+
+  const fetchPerson = async () => {
+    setLoading(true);
+
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    const person = data.results[0];
+    
+    const { phone, email } = person;
+    const { age } = person.dob;
+    const { large: image } = person.picture;
+    const { first, last } = person.name;
+    const { street: { number, name }} = person.location;
+    const { password } = person.login;
+
+    const newPerson = {
+      image,
+      phone,
+      email,
+      age,
+      password,
+      street: `${number} ${name}`,
+      name: `${first} ${last}` 
+    };
+
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle('name');
+    setValue(newPerson.name);
+  };
+
+  useEffect(() => {
+    fetchPerson();
+  }, [])
+
+  const handleValue = (e) => {
+    if(e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label;
+
+      setTitle(newValue);
+      setValue(person[newValue]);
+    }
+  }
+
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img 
+            src={(person && person.image) || defaultImage} 
+            alt="random user" 
+            className="user-img" 
+          />
+          <p className="user-title">My {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+
+            <button 
+              className="icon" 
+              data-label="name" 
+              onMouseOver={handleValue}>
+              <FaUser />
+            </button>
+            <button 
+              className="icon" 
+              data-label="email" 
+              onMouseOver={handleValue}>
+              <FaEnvelopeOpen />
+            </button>
+            <button 
+              className="icon" 
+              data-label="age" 
+              onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button 
+              className="icon" 
+              data-label="street" 
+              onMouseOver={handleValue}>
+              <FaMap />
+            </button>
+            <button 
+              className="icon" 
+              data-label="phone" 
+              onMouseOver={handleValue}>
+              <FaPhone />
+            </button>
+            <button 
+              className="icon" 
+              data-label="password" 
+              onMouseOver={handleValue}>
+              <FaLock />
+            </button>
+          </div>
+          
+          <button className="btn" type="button" onClick={fetchPerson}>{loading ? 'loading...' : 'random user'}</button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App;
